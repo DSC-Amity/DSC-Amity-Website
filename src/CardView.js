09208abcc30c31401a243ef52974c9184afd3274
shivang02 <shivang.dubey1@student.amity.edu>
2020-09-17 11:40:27 +0530

    Added basic elements to CardView

--- src/CardView.js
+++ src/CardView.js
@@ -3,7 +3,10 @@ import 'CardView.css'
 export default function CardView() {
     return (
         <div className="container">
-            
+            <img src={"src\assets\icon.jpg"} className="cardImg" id={}/>
+            <p className="CardName">Name</p>
+            <i>LinkedIn</i>
+            <i>Github</i>
         </div>
     )
 }
