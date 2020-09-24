ac866a8131508515a6bfd16b81c670cf0ec5f855
shivang02 <shivang.dubey1@student.amity.edu>
2020-09-24 17:04:59 +0530

    Added some files

---
+++ src/Updates.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Updates() {
+    return (
+        <div className="section col-3" id="updates" >
+            
+            <a class="twitter-timeline" data-height="500" data-theme="dark" href="https://twitter.com/DscAmity?ref_src=twsrc%5Etfw">Tweets by DscAmity</a>
+        </div>
+    )
+}
