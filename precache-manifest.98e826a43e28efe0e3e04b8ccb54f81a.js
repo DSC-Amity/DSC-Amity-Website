ab3c8dfed95d771af8e3bc30f580020ed99cf63b
srivastavayushi <srivastavayushi@users.noreply.github.com>
2020-10-03 13:43:53 +0530

    Updates

---
+++ precache-manifest.98e826a43e28efe0e3e04b8ccb54f81a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52683db543f460f42cdfbbbcae263da3",
+    "url": "/DSC-Amity-website/index.html"
+  },
+  {
+    "revision": "1fe3a3bd1fbf873492db",
+    "url": "/DSC-Amity-website/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "07e0367a39c4c709f5ec",
+    "url": "/DSC-Amity-website/static/css/main.dba98412.chunk.css"
+  },
+  {
+    "revision": "1fe3a3bd1fbf873492db",
+    "url": "/DSC-Amity-website/static/js/2.70969edc.chunk.js"
+  },
+  {
+    "revision": "492d7e05a4c2e81da7932422a4d31130",
+    "url": "/DSC-Amity-website/static/js/2.70969edc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07e0367a39c4c709f5ec",
+    "url": "/DSC-Amity-website/static/js/main.e85712e7.chunk.js"
+  },
+  {
+    "revision": "d6c0d774bafef615c09d",
+    "url": "/DSC-Amity-website/static/js/runtime-main.523fa762.js"
+  },
+  {
+    "revision": "7e29ef0b4c2e1d2833ff3629d51497b2",
+    "url": "/DSC-Amity-website/static/media/6p0yst.7e29ef0b.jpg"
+  },
+  {
+    "revision": "7aae5de2794aa859527bc14a02289abf",
+    "url": "/DSC-Amity-website/static/media/Checkmark.7aae5de2.png"
+  },
+  {
+    "revision": "906620933b10b35de54bd187e42ac913",
+    "url": "/DSC-Amity-website/static/media/Logo.90662093.png"
+  },
+  {
+    "revision": "dcb01086ae884d0f2f5eeb37bc02d74c",
+    "url": "/DSC-Amity-website/static/media/Lower-Shapes.dcb01086.png"
+  },
+  {
+    "revision": "503f0d1a71a6dfd624b8dc4cdd7fd290",
+    "url": "/DSC-Amity-website/static/media/Upper-Shapes.503f0d1a.png"
+  },
+  {
+    "revision": "f9e9761e3d563791cd8e4dec26d01d22",
+    "url": "/DSC-Amity-website/static/media/communities.f9e9761e.jpg"
+  }
+]);
